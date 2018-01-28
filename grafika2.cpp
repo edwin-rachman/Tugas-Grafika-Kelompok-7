18b617e26259f126f0ff4d18d25d5abd298b0b2c
edwin-rachman <13515042@std.stei.itb.ac.id>
2018-01-28 19:11:42 +0700

    Add text writer code in grafika2

--- grafika2.cpp
+++ grafika2.cpp
@@ -1,6 +1,8 @@
 #include <ctime>
 #include <fstream>
 #include <iostream>
+#include <sstream>
+#include <map>
 #include "Line.h"
 #include "Drawing.h"
 #include "CharBuilder.h"
@@ -11,7 +13,7 @@ int main() {
   FrameBuffer frameBuffer("/dev/fb0");
   
   
-  Drawing drawing1(100, 0);
+  /*Drawing drawing1(100, 0);
   drawing1.add(new Line(0, 0, 100, 100, 0xFF, 0xFF, 0xFF));
   drawing1.add(new Line(100, 0, 100, 100, 0xFF, 0xFF, 0xFF));
   drawing1.add(new Line(0, 0, 100, 0, 0xFF, 0xFF, 0xFF));
@@ -19,13 +21,45 @@ int main() {
   Drawing drawing2(200, 0);
   drawing2.add(new Line(0, 0, 100, 100, 0xFF, 0xFF, 0xFF));
   drawing2.add(new Line(100, 0, 100, 100, 0xFF, 0xFF, 0xFF));
-  drawing2.add(new Line(0, 0, 100, 0, 0xFF, 0xFF, 0xFF));
+  drawing2.add(new Line(0, 0, 100, 0, 0xFF, 0xFF, 0xFF));*/
 
+  
+  //drawing.add(&drawing1);
+  //drawing.add(&drawing2);
+  //drawing.add(BuildChar(350, 0, "font/a.txt", 80, 100, 0xFF, 0x00, 0x00));
+  
   Drawing drawing(0, frameBuffer.getHeight());
-  drawing.add(&drawing1);
-  drawing.add(&drawing2);
-  drawing.add(BuildChar(350, 0, "font/a.txt", 80, 100, 0xFF, 0x00, 0x00));
-
+  
+  int letter_width = 70;
+  int letter_height = 90;
+  std::map<char, Drawing *> letters;
+  for (char c = 'a'; c <= 'u'; ++c) {
+    std::stringstream font_file;
+    font_file << "font/" << c << ".txt";
+    letters[c] = BuildChar(0, 0, font_file.str().c_str(), letter_width, letter_height, 0xFF, 0x00, 0x00);
+  }
+  
+  std::string text = "abcde fg\na";
+  int x = 0;
+  int y = 0;
+  int width = 80;
+  int height = 100;
+  for (auto& c : text) {
+    if (c == '\n') {
+      x = 0;
+      y += height;
+    }
+    else if (c == ' ') {
+      x += width;
+    }
+    else {
+      Drawable *letter = letters[c]->clone();
+      letter->transform(x, y);
+      drawing.add(letter);
+      x += width;
+    }
+  }
+  
   
   timespec delay, rem;
   delay.tv_sec = 0;
