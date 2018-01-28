7d50be345ef1ef1db63bca0136165e61326394d5
Louis Leslie <spy.tech23@gmail.com>
2018-01-28 05:16:01 -0500

    fixed chalbuilder

--- CharBuilder.cpp
+++ CharBuilder.cpp
@@ -1,15 +1,17 @@
 #include "CharBuilder.h"
+#include "Line.h"
 #include <iostream>
+#include <fstream>
 
-Drawing * BuildChar(int x, int y, const char * filename, int width, int height, u_int8_t r, u_int8_t, g, u_int8_t, b) {
+Drawing * BuildChar(int x, int y, const char * filename, int width, int height, u_int8_t r, u_int8_t g, u_int8_t b) {
     Drawing * drawing = new Drawing(x, y);
-    ifstream stream(filename)
+    std::ifstream stream(filename);
     int count;
-    ifstream >> count;
+    stream >> count;
     for (int i = 0; i < count; i++) {
         float x1, y1, x2, y2;
-        ifstream >> x1 >> y1 >> x2 >> y2;
-        drawing.add(new Line((int)x1 * width, (int)y1 * height), (int)x2 * width, (int)y2 * height);
+        stream >> x1 >> y1 >> x2 >> y2;
+        drawing->add(new Line((int)x1 * width, (int)y1 * height, (int)x2 * width, (int)y2 * height, r, g, b));
     }
     return drawing;
 }
